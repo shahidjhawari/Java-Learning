6534bc14668ed4bdeb021ac7624bf1311e3d0828
Shahid Iqbal <shahidiqbaljhawari@gmail.com>
2026-06-05 21:09:37 +0500

    Refactor Main class to instantiate Student and display details

--- Main.java
+++ Main.java
@@ -1,9 +1,23 @@
+class Student {
+
+    String name;
+    int age;
+
+    void display() {
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+    }
+}
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("Hello World");
+        Student s1 = new Student();
 
+        s1.name = "Ali";
+        s1.age = 20;
+
+        s1.display();
     }
-
 }
